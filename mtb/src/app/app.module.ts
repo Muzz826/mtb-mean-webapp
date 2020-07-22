fc3534c2e88ef25723b3b495fb2586a213ed41f7
Ben <27180427+Muzz826@users.noreply.github.com>
2020-07-21 19:35:21 -0500

    start of posts section of blog

--- mtb/src/app/app.module.ts
+++ mtb/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
+import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
+import { NgModule } from '@angular/core';
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModulesPro.forRoot(),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
