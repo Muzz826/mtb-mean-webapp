5e7ae90b01ce9143fb6ca3d4ae486efbe3cd94ed
Ben Muzzy <muzz826@gmail.com>
2020-07-23 19:24:00 -0500

    continued building post modules

--- mtb/src/app/app.module.ts
+++ mtb/src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { MDBBootstrapModulesPro, MDBSpinningPreloader } from 'ng-uikit-pro-standard';
+import { MDBBootstrapModulesPro, MDBSpinningPreloader, AccordionModule, WavesModule } from 'ng-uikit-pro-standard';
 import { NgModule } from '@angular/core';
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { FormsModule } from '@angular/forms';
 import { MdbWysiwygModule } from 'mdb-wysiwyg';
+import { HeaderComponent } from "./header/header.component";
+import { PostListComponent } from "./posts/post-list/post-list.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
+    HeaderComponent,
+    PostListComponent,
   ],
   imports: [
     BrowserModule,
     MDBBootstrapModulesPro.forRoot(),
     FormsModule,
-    MdbWysiwygModule, 
+    MdbWysiwygModule,
+    AccordionModule,
+    WavesModule,
   ],
   providers: [MDBSpinningPreloader],
   bootstrap: [AppComponent]
--- mtb/src/app/posts/post-create/post-create.component.ts
+++ mtb/src/app/posts/post-create/post-create.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, Output } from "@angular/core";
 @Component({
   selector: "app-post-create",
   templateUrl: "./post-create.component.html",
-  styleUrls: ["./post-create.component.css"]
+  styleUrls: ["./post-create.component.scss"]
 })
 export class PostCreateComponent {
   enteredValue = '';
