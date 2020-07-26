d5837ce28b539fc9b0fc1460680329fef28afd3a
Ben Muzzy <muzz826@gmail.com>
2020-07-25 20:51:53 -0500

    continued work on posting components

--- mtb/src/app/posts/post-create/post-create.component.ts
+++ mtb/src/app/posts/post-create/post-create.component.ts
@@ -1,23 +1,17 @@
-import { Component, EventEmitter, Output, OnInit } from "@angular/core";
-import { NgForm, FormGroup, FormControl, Validators } from "@angular/forms";
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 import { PostsService } from "../posts.service";
-import { Post } from '../post.model';
+
 @Component({
   selector: "app-post-create",
   templateUrl: "./post-create.component.html",
   styleUrls: ["./post-create.component.scss"]
 })
-export class PostCreateComponent implements OnInit {
-  validatingForm: FormGroup;
-
-  ngOnInit() {
-    this.validatingForm = new FormGroup({
-      required: new FormControl(null, Validators.required)
-    });
-
+export class PostCreateComponent {
+  
   enteredTitle = "";
   enteredContent = "";
-  @Output() postCreated = new EventEmitter<Post>();
+  
 
   constructor(public postsService: PostsService) {}
 
@@ -25,10 +19,7 @@ export class PostCreateComponent implements OnInit {
     if (form.invalid) {
       return;
       }
-      const post: Post = {
-        title: form.value.title,
-        content: form.value.content,
-      };
-      this.postCreated.emit(post);
+      this.postsService.addPost(form.value.title, form.value.content);
+      form.resetForm();
   }
 }
