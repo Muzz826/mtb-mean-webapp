a565a879b0503b947b1b022826cfad03bd223675
Ben Muzzy <muzz826@gmail.com>
2020-07-28 15:07:15 -0500

    starting setting up navigation/routes

--- mtb/backend/app.js
+++ mtb/backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 
-const Post = require('./models/post');
+const postsRoutes = require("./routes/posts");
 const app = express();
 
 mongoose.connect("mongodb+srv://db-admin:iNRwSQfh1Vn1kaIs@cluster0.qcbva.mongodb.net/mtb?retryWrites=true&w=majority")
@@ -24,39 +24,11 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: "Post added successfully",
-      postId: createdPost._id
-    });
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then(documents => {
-    console.log(documents);
-    res.status(200).json({
-      message: "Posts fetched successfully!",
-      posts: documents
-  });
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({ message: "Post deleted!" });
-  });
-});
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
--- mtb/src/app/posts/post-create/post-create.component.ts
+++ mtb/src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Post } from '../post.model';
 import { PostsService } from "../posts.service";
 
 @Component({
@@ -7,19 +9,45 @@ import { PostsService } from "../posts.service";
   templateUrl: "./post-create.component.html",
   styleUrls: ["./post-create.component.scss"]
 })
-export class PostCreateComponent {
-  
+export class PostCreateComponent implements OnInit{
+
   enteredTitle = "";
   enteredContent = "";
-  
+  post: Post;
+  private mode = 'create';
+  private postId: string;
 
-  constructor(public postsService: PostsService) {}
 
-  onAddPost(form: NgForm) {
+  constructor(public postsService: PostsService, public route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.postsService.getPost(this.postId).subscribe(postData => {
+          this.post = { id: postData._id, title: postData.title, content: postData.content};
+        });
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    })
+  }
+
+  onSavePost(form: NgForm) {
     if (form.invalid) {
       return;
-      }
+    }
+    if (this.mode === 'create') {
       this.postsService.addPost(form.value.title, form.value.content);
-      form.resetForm();
+    } else {
+      this.postsService.updatePost(
+        this.postId,
+        form.value.title,
+        form.value.content
+      );
+    }
+    form.resetForm();
   }
 }
