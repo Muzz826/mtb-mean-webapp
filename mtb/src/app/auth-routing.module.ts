7dc9905ed2c418effb8c57ed5aa8e13c2cb2295f
Ben Muzzy <muzz826@gmail.com>
2020-08-01 19:19:17 -0500

    added more components, styling, etc.

--- mtb/src/app/auth-routing.module.ts
+++ mtb/src/app/auth-routing.module.ts
@@ -3,27 +3,27 @@ import { TechReviewsComponent } from './tech-reviews/tech-reviews.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 // This route module is for routing once logged in only.
 
 const routes: Routes = [
   // path and component for login page
-  { path: "login", component: LoginComponent },
+  { path: 'login', component: LoginComponent },
   // path and component for signup page
-  { path: "signup", component: SignupComponent },
+  { path: 'signup', component: SignupComponent },
   // path and component for home page
-  { path: "home", component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   // path and component for about page
-  { path: "about", component: AboutComponent },
+  { path: 'about', component: AboutComponent },
   // path and component for contact page
-  { path: "contact", component: ContactComponent },
+  { path: 'contact', component: ContactComponent },
   // path and component for tech reviews
-  { path: "tech-reviews", component: TechReviewsComponent },
+  { path: 'tech-reviews', component: TechReviewsComponent },
   // path and component for tech tutorials
-  { path: "tech-tutorials", component: TechTutorialsComponent },
+  { path: 'tech-tutorials', component: TechTutorialsComponent },
 
 
 ];
--- mtb/src/app/posts/post-list/post-list.component.ts
+++ mtb/src/app/posts/post-list/post-list.component.ts
@@ -1,22 +1,17 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
-import { PageEvent } from "@angular/material/paginator";
-import { Subscription } from "rxjs";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { Subscription } from 'rxjs';
 
-import { Post } from "../post.model";
-import { PostsService } from "../posts.service";
-import { AuthService } from "../../auth/auth.service";
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
-  selector: "app-post-list",
-  templateUrl: "./post-list.component.html",
-  styleUrls: ["./post-list.component.scss"]
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.scss']
 })
 export class PostListComponent implements OnInit, OnDestroy {
-  // posts = [
-  //   { title: "First Post", content: "This is the first post's content" },
-  //   { title: "Second Post", content: "This is the second post's content" },
-  //   { title: "Third Post", content: "This is the third post's content" }
-  // ];
   posts: Post[] = [];
   isLoading = false;
   totalPosts = 0;
